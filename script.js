e3da3d870e3fa96fee7f1efce00c1ab764c6de56
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-06-29 15:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,39 @@ function convertToRoman(num) {
     };
 
   //your code here
+	function convertToRoman(num) {
+    const romanSymbols = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+
+    let roman = '';
+
+    for (const [symbol, value] of romanSymbols) {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
+}
+
+// Examples:
+console.log(convertToRoman(14));  // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
